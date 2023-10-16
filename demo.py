c85e4bcfb0cb6d4f6608dca4057a3be5f8b20da8
SAMLab <147920838+GZU-SAMLab@users.noreply.github.com>
2023-10-16 20:00:38 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,19 +4,18 @@ from torchvision import transforms
 from PIL import Image
 from torchsummary import summary
 
-from  model_vgg16_ablation import *
-from backbone_vgg16 import *
+from  model.model_vgg16_ import *
 
 
 #定义训练设备
-device = torch.device("cpu")
+device = torch.device("cuda")
 
 #创建网络模型
-model = Multi_Granularity(device="cpu")
-model.load_state_dict(torch.load("\\best_weight\CSNet_best_weight.pth", map_location="cpu"))
+model = Multi_Granularity(decive=device)
+model.load_state_dict(torch.load("\\best_weight\CSNet_best_weight.pth"))   #权重路径
 model.to(device)
 model.eval()
-img_path = "D:\python_project\practice\count_w\MLP\\test\\0addc041-a6b6-4643-8e10-8b9c51e932f1.png"
+img_path = "D:\python_project\practice\count_w\MLP\\test\\0addc041-a6b6-4643-8e10-8b9c51e932f1.png"  #图片路径
 image = Image.open(img_path)
 image = image.convert("RGB")
 transform = transforms.ToTensor()
